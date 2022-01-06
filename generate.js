66198fa1bc52a30bca11c87248b556597e7c9a4e
Mariana Marques <65464431+TheQueenofEverything@users.noreply.github.com>
2022-01-06 16:34:42 -0500

    Add files via upload

---
+++ generate.js
@@ -0,0 +1,25 @@
+//Elements
+const password = document.querySelector ('#password');
+const button = document.querySelector('button');
+
+//Password Data
+let characters = '0123456789abcdfghijklmnopqrstuvxywzABCDEFGHIJKLMNOPQRSTUVXYWZ!@#$%^&*()_+[]{}|:"/?<>`~';
+let passwordLength = 30;
+let passwordValue = ''; 
+
+//Creating the Password 
+const createPassword = () => {
+    passwordValue = '';
+
+    for (let i = 0; i < passwordLength; i++) {
+        let number = Math.floor(Math.random() * characters.length);
+        passwordValue += characters.substring(number, number +1);
+
+}
+
+    password.value = passwordValue;
+
+}
+
+//Event - Will activate the action ' Click ' - 
+button.addEventListener('click', createPassword);
